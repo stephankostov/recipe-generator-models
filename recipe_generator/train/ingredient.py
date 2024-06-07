aa28f40300aedfaff0bc262b4a9250569f05c680
stephankostov <stephank179@gmail.com>
2024-06-07 12:16:17 +0100

    updating to more efficient implementation of GPT

--- recipe_generator/train/ingredient.py
+++ recipe_generator/train/ingredient.py
@@ -19,7 +19,7 @@ import torch.nn as nn
 
 import wandb
 
-from recipe_generator.models.ingredient_simple import IngredientModelTorch
+from recipe_generator.models.ingredient_simple import IngredientModel
 import recipe_generator.datasets as datasets
 import recipe_generator.optimiser as optimiser
 from recipe_generator.trainer import Trainer
@@ -50,9 +50,9 @@ def main(recipes_file='../data/local/final/full/recipes/recipe_food_ids.npy',
     np.random.shuffle(recipes)
     data_train, data_validation = recipes[:int(cv_ratio*len(recipes))], recipes[int(cv_ratio*len(recipes)):]
     train_ds, validation_ds = datasets.NextTokenDataset(data_train, model_cfg.block_size, device=train_cfg.device), datasets.NextTokenDataset(data_validation, model_cfg.block_size, device=train_cfg.device), 
-    train_dl, validation_dl = DataLoader(train_ds, batch_size=train_cfg.batch_size, shuffle=True, num_workers=2), DataLoader(validation_ds, batch_size=train_cfg.batch_size, shuffle=False, num_workers=2)
+    train_dl, validation_dl = DataLoader(train_ds, batch_size=train_cfg.batch_size, shuffle=True), DataLoader(validation_ds, batch_size=train_cfg.batch_size, shuffle=False)
 
-    model = IngredientModelTorch(model_cfg, embedding_weights)
+    model = IngredientModel(model_cfg, embedding_weights)
     model.to(train_cfg.device)
 
     if debugger_is_active(): train_cfg.wandb = False
